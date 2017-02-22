265dc43f7fb0986d4e0911648eee13ec139c3e14
Unknown <kinzie.colleen@gmail.com>
2017-02-22 11:16:13 -0700

    Added Lists Unity File

---
+++ Lists/Assets/Lists.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour {
+
+	public List<string> myWeaponList; //lists are not a function, but an object, and are updated arrays. They can change
+	public string[] myWeaponArray;
+
+	// Use this for initialization
+	void Start () {
+		myWeaponList.Add("Sword");
+	//	myWeaponList.Remove("Axe");
+		myWeaponList.Add("Bow");
+	//	myWeaponList.RemoveAt(1);
+	}
+	public void AddtoList () {
+		myWeaponList.Add("Rock");
+	}
+}
