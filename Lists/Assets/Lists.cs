d931131012ce57e5072746c90d2c257a24368f97
Unknown <kinzie.colleen@gmail.com>
2017-02-22 23:00:34 -0700

    Added List Items

--- Lists/Assets/Lists.cs
+++ Lists/Assets/Lists.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Lists : MonoBehaviour {
 
+//	public List<string> myWeaponsList;
+//	public List<string> myItemsList;
+	public List<string> myFlowersList;
+
 	// Use this for initialization
+//	void Start () {
+//		myWeaponsList.Add("Sword"); //Will add a fourth element called "Sword"
+//		myWeaponsList.Remove("Axe"); //Will remove the element called "Axe"
+   	//	myWeaponsList.Add("Bow"); //Will create, again, a fourth element, called "Bow"
+  	//	myWeaponsList.RemoveAt(1); //Will remove whatever the first element is regardless
+//	}
+//	void Start () {
+//		myItemsList.Add("Health Booster");
+//		myItemsList.Add("Tome");
+//		myItemsList.Remove("Stat Booster");
+//	}
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		myFlowersList.Add("Tiger lillies");
+		myFlowersList.RemoveAt(2);
+		myFlowersList.Add("Daffodils");
 	}
 }
